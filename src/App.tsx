eaf0478e97e1212b5f26e5ec01c376b6994d8103
Nascccom <ostrov_o@bk.ru>
2022-11-05 11:33:49 +0300

    First commit and add .idea to ignore

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr"/>
+      </header>
+      <nav className="nav">
+        <div>Profile</div>
+        <div>Message</div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </nav>
+      <main className="content">
+
+      </main>
+    </div>);
+}
+
+
+export default App;
